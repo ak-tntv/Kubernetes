bf67329f8a9d85de78ec54050363a4e58a46cc3e
Arjan Kolkman <arjan.kolkman@gmail.com>
2019-01-17 16:32:25 +0100

    Initial checkin

---
+++ install-pod-network.sh
@@ -0,0 +1,3 @@
+export KUBECONFIG=/etc/kubernetes/admin.conf
+kubectl apply -f https://docs.projectcalico.org/v3.1/getting-started/kubernetes/installation/hosted/canal/rbac.yaml
+kubectl apply -f https://docs.projectcalico.org/v3.1/getting-started/kubernetes/installation/hosted/canal/canal.yaml
